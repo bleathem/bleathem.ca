5ce2803f180bd706452f28ceaa428f2d449b2be3
Brian Leathem <bleathem@gmail.com>
2012-05-08 21:46:20 -0700

    Fixed the blog folder/layout

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,6 +1,5 @@
 
 Awestruct::Extensions::Pipeline.new do
-  extension Awestruct::Extensions::Posts.new( '/news', :posts )
+  extension Awestruct::Extensions::Posts.new( '/posts', :posts )
   # extension Awestruct::Extensions::Indexifier.new
 end
-

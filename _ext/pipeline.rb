d1d8523683ed0538c279e83f9e816e1909a669f1
Brian Leathem <bleathem@gmail.com>
2012-05-11 13:39:12 -0700

    Added a paginator to the index page

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -4,5 +4,6 @@ require_relative 'posts_map'
 Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new( '/posts', :posts )
   extension PostsMap.new
+  extension Awestruct::Extensions::Paginator.new( :posts, '/index', :per_page=>7 )
   # extension Awestruct::Extensions::Indexifier.new
 end

7cbb9293c92c0949b4c3b3eed574f0d714dd12d3
Brian Leathem <bleathem@gmail.com>
2012-05-16 14:19:35 -0700

    Added an atom RSS feed

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -8,5 +8,5 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Paginator.new( :posts, '/index', :per_page=>7 )
   extension Awestruct::Extensions::Tagger.new( :posts, '/index', '/blog/tags', :per_page=>10 )
   extension Awestruct::Extensions::TagCloud.new( :posts, 'blog/tags/index.xhtml')
-  #extension Awestruct::Extensions::Atomizer.new( :posts, '/feed.atom', :feed_title=>'Java EE - Experiences of a Developer | by Brian Leathem' )
+  extension Awestruct::Extensions::Atomizer.new( :posts, '/feed.atom', :feed_title=>'Java EE - Experiences of a Developer | by Brian Leathem' )
 end
